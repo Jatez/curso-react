f03732693457274fa3d5625f2f9ae0617c982062
Jader Daniel Atehortua Zapata <daniel0606zapata@gmail.com>
2022-12-23 15:37:58 -0500

    - Manejo de eventos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,28 @@ import './TodoItem.css'
 
 
 function TodoItem(props){
+    const onComplete = () => {
+        alert('Ya se completó el Todo ' + props.text);
+    };
+
+    const onDelete = () => {
+        alert('Se borró la tarea ' + props.text);
+    };
+
     return(
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>C</span>
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}>
+                -
+            </span>
+
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <span 
+                className="Icon Icon-delete" 
+                onClick={onDelete}>
+                X
+            </span>
         </li>
     );
 }
